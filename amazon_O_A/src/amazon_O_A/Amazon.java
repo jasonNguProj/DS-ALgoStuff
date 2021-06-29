e9f213205881307e897afe89ade587552e19e2f7
nguja <nguja@192.168.0.18>
2021-06-29 13:14:05 -0400

    DS/ALgo stuff

---
+++ amazon_O_A/src/amazon_O_A/Amazon.java
@@ -0,0 +1,36 @@
+package amazon_O_A;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Amazon {
+
+	public static Set<Integer> findSet(int[] arr) {
+		long totalSum = 0;
+		int aSum = 0;
+		Set<Integer> result = new TreeSet<>();
+		for (int i : arr) {
+			totalSum += i;
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i = arr.length - 1 ; i >= 0; i--) {
+			result.add(arr[i]);
+			aSum += arr[i];
+			if(aSum > totalSum - aSum) break;
+		}
+		return result;
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Set<Integer> vals = findSet(new int[] {3,7,5,6,2});
+		System.out.println(findSet(new int[] {3,7,5,6,2}));
+	}
+
+}
